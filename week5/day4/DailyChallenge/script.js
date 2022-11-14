2a2956b5616c3adc62983e535bba86a6690731c9
Eddyyemmy <edwardjaleyemi@gmail.com>
2022-11-14 23:52:54 +0200

    working on a med libs game, where words are filled in

---
+++ week5/day4/DailyChallenge/script.js
@@ -0,0 +1,31 @@
+// In todays exercise we will be creating a Mad Libs game.
+// If you’ve never played this game, a mad lib is a game where you fill in a bunch of blank inputs with different word types (ie : noun, adjective, verb), and then a story is generated based on the words you chose, and sometimes the story is surprisingly funny.
+
+// In this exercise you work with the HTML code presented below.
+
+// Follow these steps :
+
+// Get the value of each of the inputs in the HTML file when the form is submitted. Remember the event.preventDefault()
+const button = getButton()
+button?.addEventListener("click", handleClick)
+
+// Write a story that uses each of the values.
+// Make sure you check the console for errors when playing the game.
+// Bonus: Add a “shuffle” button to the HTML file, when clicked the button should change the story currently displayed (but keep the values entered by the user). The user could click the button at least three times and get a new story. Display the stories randomly.
+
+function handleClick(e) {
+    e.preventDefault()
+    const noun = document.getElementById("noun").value
+    const adjectice = document.getElementById("adjective").value
+    const person = document.getElementById("person").value
+    const verb = document.getElementById("verb").value
+    const place = document.getElementById("place").value
+    
+    // Make sure the values are not empty
+    if (noun == "" || adjective == "" || person == "" || verb == "" || place == "") return
+    console.log("all fields are filled in, cool !")
+}
+
+function getButton() {
+    return document.getElementById("lib-button")
+}
