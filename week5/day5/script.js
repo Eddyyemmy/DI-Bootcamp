d0f48e10ff37955e57ed76c7f9a7fb803d8b2482
Eddyyemmy <edwardjaleyemi@gmail.com>
2022-11-16 02:05:14 +0200

    creating mouseup, down move functions

--- week5/day5/script.js
+++ week5/day5/script.js
@@ -1,15 +1,49 @@
 generateColors()
+let currentColor
+listenToMain()
+let isMousePressed = false
+
+function listenToMain() {
+    const main =document.getElementById("main")
+    main.addEventListener("mousedown", handleClick)
+    main.addEventListener("mousemove", handleMove)
+    document.body.addEventListener("mouseup", handleMouseRelease)
+}
+
+function handleMouseRelease() {
+    isMousePressed = false
+}
+
+function handleClick() {
+    if (currentColor == null) return
+    isMousePressed = true
+}
+
+function handleMove () {
+    if (!isMousePressed) return
+    console.log("ds")
+}
+
+
 
 function generateColors() {
     const colorDivs = document.querySelectorAll(".color")
    for (const colorDiv of colorDivs) {
     const randomColor = generateRandomColor()
     colorDiv.style.backgroundColor = randomColor
+    colorDiv.dataset.color = randomColor
+    colorDiv.addEventListener("click", setCurrentColor)
    }
 }
 
-// Color generator function
+
+
+// randomColor generator function
 function generateRandomColor() {
     const hex = Math.floor(Math.random() * 16777215).toString(16)
     return "#" + hex 
+}
+ 
+function setCurrentColor(e) {
+    currentColor = e.target.dataset.color
 }
