ac8c3bf483b00f04523f736b7db338b791fdd22a
Eddyyemmy <edwardjaleyemi@gmail.com>
2022-11-16 22:13:43 +0200

    adding checkbox and font awesome feature to todo list

--- week5/day5/DailyChallenge/script.js
+++ week5/day5/DailyChallenge/script.js
@@ -1,22 +1,51 @@
 // In the js file, you must add the following functionalities:
 // Create an empty array : const tasks = [];
-const tasks = [];
 
 // Create a function called addTask(). As soon as the user clicks on the button:
 
 const form = document.getElementById("form");
-form.addEventListener("submit", addTask);
+const ul = document.getElementById("list-tacks");
 
-function addTask(e) {
-    e.preventDefault();
+form.addEventListener("submit", addTask);
+const tasks = [];
+
+function addTask(event) {
+    event.preventDefault();
     const formData = new FormData(form);
     const task = formData.get("task");
-    if (task === "") return
+    if (task === "") return;
     tasks.push(task);
+    renderTasks(tasks);
     form.reset();
-    console.log("form submitted", tasks);
-
 }
+
+
+
+function renderTasks(tasks) {
+    console.log(tasks);
+    ul.innerHTML = "";
+    
+    for (const task of tasks) {
+        const li = generateLi(task);
+        ul.append(li);
+    }
+}
+
+function generateLi(task) {
+    const li = document.createElement("li");
+    const xMark = document.createElement("i");
+    xMark.classList.add("fa-solid", "fa-xmark");
+    const input = document.createElement("input");
+    input.setAttribute("type", "checkbox");
+    const span = document.createElement("span");
+    span.innerText = task;
+    
+
+    li.append(xMark, input, span);
+    return li;
+}
+
+
 // check that the input is not empty,
 // then add it to the array (ie. add the text of the task)
 // then add it to the DOM, below the form (in the <div class="listTasks"></div>) .
