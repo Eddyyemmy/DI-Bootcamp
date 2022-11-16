881826702756afd66db761da38ad1b2170aadac0
Eddyyemmy <edwardjaleyemi@gmail.com>
2022-11-16 09:49:50 +0200

    adding color picking dawing using js functions and loops

--- week5/day5/script.js
+++ week5/day5/script.js
@@ -3,6 +3,22 @@ let currentColor
 listenToMain()
 let isMousePressed = false
 
+const button = document.querySelector(".clear button")
+listenToClearButton()
+
+function listenToClearButton() {
+    button.addEventListener("click", clearCanvas)
+}
+
+function clearCanvas() {
+    currentColor = null
+    const squares = document.querySelectorAll("#main > div")
+    for (const square of squares) {
+        square.style.backgroundColor = "white"
+    }
+    button.style.backgroundColor = "color"
+}
+
 function listenToMain() {
     const main =document.getElementById("main")
     main.addEventListener("mousedown", handleClick)
@@ -19,20 +35,19 @@ function handleClick() {
     isMousePressed = true
 }
 
-function handleMove () {
+function handleMove (e) {
     if (!isMousePressed) return
-    console.log("ds")
+    const hoverdDiv = document.elementFromPoint(e.x, e.y)
+    hoverdDiv.style.backgroundColor = currentColor
 }
-
-
 
 function generateColors() {
     const colorDivs = document.querySelectorAll(".color")
-   for (const colorDiv of colorDivs) {
-    const randomColor = generateRandomColor()
-    colorDiv.style.backgroundColor = randomColor
-    colorDiv.dataset.color = randomColor
-    colorDiv.addEventListener("click", setCurrentColor)
+    for (const colorDiv of colorDivs) {
+        const randomColor = generateRandomColor()
+        colorDiv.style.backgroundColor = randomColor
+        colorDiv.dataset.color = randomColor
+        colorDiv.addEventListener("click", setCurrentColor)
    }
 }
 
