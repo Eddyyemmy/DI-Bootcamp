0301a11bdaacc11e7d698c9230f294270a153511
Eddyyemmy <edwardjaleyemi@gmail.com>
2022-11-18 20:41:16 +0200

    moving function, with interval

---
+++ week6/day2/ExercisesXP/Exercise2/script.js
@@ -0,0 +1,16 @@
+let distance = 0
+let interval
+
+function myMove() {
+   interval = setInterval(moveRedSquare, 1)
+}
+
+function moveRedSquare() {
+    const WIDTH_OF_CONTAINER = 400
+    const WIDTH_OF_ANIMATE = 50
+ 
+    if (distance === WIDTH_OF_CONTAINER - WIDTH_OF_ANIMATE) return clearInterval(interval)
+    distance = distance + 1
+    const redSquare = document.getElementById("animate")
+    redSquare.style.left = distance + "px";
+}
