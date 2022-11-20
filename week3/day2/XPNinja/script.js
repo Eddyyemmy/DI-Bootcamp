9eef7b78f805cc50156d405769a4ee8b010b9d3d
Eddyyemmy <edwardjaleyemi@gmail.com>
2022-11-20 16:42:34 +0200

    modified

---
+++ week3/day2/XPNinja/script.js
@@ -0,0 +1,15 @@
+// console.log(5 >= 1)
+// console.log(0 === 1)
+// console.log(4 <= 1)
+// console.log(1 != 1)
+// console.log("A" > "B")
+// console.log("B" < "C")
+// console.log("a" > "A")
+// console.log("b" < "A")
+// console.log(true === false)
+// console.log(true != true)
+
+let ask = prompt("adnum with comma");
+let answer = ask.join(' ')
+
+console.log(answer)
