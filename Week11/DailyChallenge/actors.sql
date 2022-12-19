7e18f51b99474846ddeb6a7af922b46f25f3012d
Eddyyemmy <edwardjaleyemi@gmail.com>
2022-12-19 18:54:22 +0200

    counting num of data input

---
+++ Week11/DailyChallenge/actors.sql
@@ -0,0 +1,19 @@
+-- CREATE TABLE actors(
+-- 	id SERIAL PRIMARY KEY,
+-- 	last_name VARCHAR(50) NOT NULL,
+-- 	first_name VARCHAR(50) NOT NULL,
+-- 	birth_day DATE NOT NULL
+-- )
+
+-- INSERT INTO actors (first_name, last_name, birth_day)
+-- VALUES('Marc', 'Benichou', '1998-11-02'),
+-- 	('Yoan', 'Cohen', '2010-12-03'),
+-- 	('Lea', 'Benichou', '1987-07-27'),
+-- 	('Amelia', 'Dux', '1996-04-07'),
+-- 	('David', 'Grez', '2003-06-14'),
+-- 	('Omer', 'Simpson', '1980-10-03')
+
+-- SELECT COUNT(*) FROM actors
+
+INSERT INTO actors (first_name, last_name, birth_day)
+VALUES('', '', '')
