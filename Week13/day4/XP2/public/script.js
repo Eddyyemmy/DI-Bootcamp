61392b193400a3e57c22447a55c041902caa3a25
Eddyyemmy <edwardjaleyemi@gmail.com>
2023-01-08 14:55:51 +0200

    modified

--- Week13/day4/XP2/public/script.js
+++ Week13/day4/XP2/public/script.js
@@ -1,25 +1,16 @@
-function sendData() {
-    const user = {
-        firstname: 'John',
-        lastname: 'Doe'
-    }
-
-    console.log(user);
-    fetch('http://localhost:3001/',{
-        method:'POST',
-        headers:{
-            'Content-Type': 'application/json'
-        },   
-        body: JSON.stringify(user)
-    })
+function fetchUser() {
+    fetch('http://localhost:3001/fetchuser')
     .then(res=> res.json())
     .then(data =>{
         document.getElementById('root').innerHTML = `${data.firstname} ${data.lastname}`
+        console.log(data);
     })
-    .catch(e => console.log(e))
+    .catch(e =>{
+      console.log(e)
+    })
 }
 
-sendData()
+fetchUser()
 
 const button = document.getElementById('button')
 button.addEventListener("click", clickMe)
