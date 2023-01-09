3978dc3e5e70e179c0ab63b9a947753aae317199
Eddyyemmy <edwardjaleyemi@gmail.com>
2023-01-09 16:47:12 +0200

    week13 reupload

--- Week13/day5/mini-project-Shopping-List/public/script.js
+++ Week13/day5/mini-project-Shopping-List/public/script.js
@@ -1,27 +1,28 @@
-const form = document.querySelector("form");
-form.addEventListener("submit", submitData)
+const form = document.querySelector('form')
+form.addEventListener('submit', submitList);
 
- function submitData(e){
-   e.preventDefault();
-   const formData = new FormData(form)
-   const cart = {};
-   for (const entry of formData) {
-     const key = entry[0];
-     const value = entry[1];
-     cart[key] = value;
-   }
-   sendCartToServer(cart);
-   getShoppingItems()
- }
+function submitList(e){
+  e.preventDefault();
+  const formData = new FormData(form);
+  const cart = {};
+  for (const entry of formData) {
+    const [key, value] = entry;
+    cart[key] = value;
+  }
+  sendListToServer(cart);
+  getList()
+}
 
- function sendCartToServer(cart) {
-   fetch('http://localhost:3000/items',{
+
+
+function sendListToServer(cart) {
+  fetch('http://localhost:3000/items',{
     method: 'POST',
-    body: JSON.stringify(cart),
     headers: {
       'Content-Type':'application/json'
     },
-    })
+    body: JSON.stringify(cart)
+  })
   .then(res => res.json())
   .then(data => {
     console.log(data);
@@ -29,23 +30,24 @@ form.addEventListener("submit", submitData)
   .catch(err => {
     console.log(err);
   })
- }
-
- function displayShoppingItems(data) {
-   let ul = document.getElementById('root');
-   ul.innerHTML = "";
-   data.forEach(item =>{
-   let li = document.createElement('li');
-   li.innerText= `${item.item}: ${item.amount}`;
-   ul.append(li)
-})
 }
 
- function getShoppingItems() {
-   fetch('http://localhost:3000/items')
+
+function getList() {
+  fetch('http://localhost:3000/items')
   .then(res => res.json())
   .then(data => {
     console.log(data);
-      displayShoppingItems(data);
+    displayShoppingList(data);
+  })
+}
+
+function displayShoppingList(data) {
+  let root = document.getElementById('root');
+  root.innerHTML = "";
+  data.forEach(item =>{
+    let li = document.createElement('li');
+    li.innerText= `${item.item}: ${item.amount}`;
+    root.append(li)
   })
 }
--- Week13/day5/mini-project-Shopping-List/server.js
+++ Week13/day5/mini-project-Shopping-List/server.js
@@ -1,27 +1,28 @@
 const exp = require('express');
-const cors = require('cors')
-const fs = require('fs');
-
+const ps = require('body-parser');
+const cors = require('cors');
 const app = exp();
 
-app.use(cors())
-app.use(exp.urlencoded({extended:false}));
-app.use(exp.json());
 
+app.use(ps.urlencoded({extended:false}));
+app.use(ps.json());
+app.use(cors());
 
-app.use('/',exp.static(__dirname+'/public'));
-  let items = [];
-app.post("/items", (req,res)=>{
-  const body = req.body;
-  const {item, amount } = body;
-  const getObject = { item, amount: Number(amount) };
-  items.push(getObject)
+app.use('/', exp.static(__dirname+'/public'));
 
-  res.send(getObject);
+let items = [];
+
+app.get('/items',(req,res)=>{
+  res.send(items);
 })
 
-app.get('/items', (req,res)=>{
-  res.send(items);
+app.post('/items',(req,res)=>{
+  const body = req.body
+  const item = body.item;
+  const amount = body.amount;
+  const toObeject = {item, amount:Number(amount)};
+  items.push(toObeject);
+  res.send(toObeject);
 })
 
 app.listen(3000);
