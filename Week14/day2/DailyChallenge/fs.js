99ead0ef0f327834ec708908243d94b032313c66
Eddyyemmy <edwardjaleyemi@gmail.com>
2023-01-09 23:05:22 +0200

    game mod

--- Week14/day2/DailyChallenge/fs.js
+++ Week14/day2/DailyChallenge/fs.js
@@ -3,16 +3,27 @@ const fs = require("fs");
 const symbols = fs.readFileSync('RightLeft.txt', 'utf8').split('')
 let displaySymbols = symbols
 console.log(displaySymbols);
-let position = 0;
 
+let position = 0;
+let step = 0;
+let setatminusone = null;
 for (const symbol of symbols) {
+  if(symbol != "<" && symbol != ">") {
+    console.log("wrong step");
+    continue;
+  }
   if (symbol === '<') {
     position = position - 1;
     const negative = position-1
   } else if (symbol === '>') {
     position = position + 1;
-  } else {
-    console.log(position);
+
+  }
+
+step++
+  if (position === -1 && setatminusone === null) {
+    setatminusone = step;
+    console.log(`First time in left side is in: ${setatminusone} steps`);
   }
 }
 
