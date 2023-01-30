0f9e1458d05448443451312737c7a49d4f3e4a0f
Eddyyemmy <edwardjaleyemi@gmail.com>
2023-01-30 18:44:46 +0200

    miniproject completion

--- Week16-React-Framework/day5/Mini-Project-To-Do-List/todo-list/src/App.js
+++ Week16-React-Framework/day5/Mini-Project-To-Do-List/todo-list/src/App.js
@@ -2,17 +2,36 @@ import React, { Component } from 'react';
 import "./App.css"; 
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state ={ todos: ["Buy some milk", "Do my homework"] };
+  }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const inputText = document.getElementById("input-text");
+    const todo = inputText.value;
+    this.setState({todos: [...this.state.todos, todo]});
+    inputText.value = "";
+  };
+
+  handleClick = (todoToDelete) => {
+    console.log(todoToDelete);
+    this.setState({ todos: this.state.todos.filter((el) => el !== todoToDelete) });
+  }
   render() {
     return (
       <div className="container">
         <h1>Todo's</h1>
         <div className="todo-list">
-          <p className="todo">Buy some milk</p>
-          <p className="todo">Do some homework</p>
+          {this.state.todos.map((todo) => (
+            <div key={todo} className="todo" onClick={() => this.handleClick(todo)}>
+                {todo}
+            </div>
+          ))}
         </div> 
-        <p className="no-todos">You have no todo left, yay!</p>
-        <form>
+        {this.state.todos.length === 0 && <p className="no-todos">You have no todo left, yay!</p>}
+        <form onSubmit={this.handleSubmit}>
           <label htmlFor="input-text">Add a new todo:</label>
           <input type="text" id="input-text" />
         </form>
